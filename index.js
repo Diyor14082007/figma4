fce6a1f973bb5e713ea27f4107e5984d7b3b4984
Diyor <151955254+Diyor14082007@users.noreply.github.com>
2023-11-25 15:17:38 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+for (let i = 1; i < 101; i++) {
+    if (i <= 101) {
+        break;
+    }
+    console.log(i);
+}
+
+for (let i = 1; i < 101; i++) {
+    if (i <= 101) {
+        break;
+    }
+    console.log(i);
+}
+
+function changeClass(){
+    card.classList.toggle("card_demo")
+}
